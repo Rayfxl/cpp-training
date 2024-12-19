6f6bd78a7683b73af5fe32d896fe28aefad3e09b
Rayfxl <ray133540@gmail.com>
2024-12-19 21:56:46 +0800

    test: Pass right command test cases

--- src/ExecutorImpl.cpp
+++ src/ExecutorImpl.cpp
@@ -33,6 +33,16 @@ void ExecutorImpl::Execute(const std::string& commands) noexcept
             } else if (pose.heading == 'S') {
                 pose.heading = 'E';
             }
+        } else if (cmd == 'R') {
+            if (pose.heading == 'E') {
+                pose.heading = 'S';
+            } else if (pose.heading == 'W') {
+                pose.heading = 'N';
+            } else if (pose.heading == 'N') {
+                pose.heading = 'E';
+            } else if (pose.heading == 'S') {
+                pose.heading = 'W';
+            }
         }
     }
 
