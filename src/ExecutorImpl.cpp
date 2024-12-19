46231e1d827fe48aac527f722a205fab32d3247c
Rayfxl <ray133540@gmail.com>
2024-12-19 21:04:31 +0800

    test: Pass initial and default pose test cases

--- src/ExecutorImpl.cpp
+++ src/ExecutorImpl.cpp
@@ -5,12 +5,15 @@ Executor* Executor::NewExecutor(const Pose& pose) noexcept
 {
     return new (std::nothrow) ExecutorImpl(pose);
 }
-ExecutorImpl::ExecutorImpl(const Pose& pose) noexcept 
+
+ExecutorImpl::ExecutorImpl(const Pose& pose) noexcept : pose(pose)
 {
 }
+
 void ExecutorImpl::Execute(const std::string& commands) noexcept
 {
 }
+
 Pose ExecutorImpl::Query() const noexcept
 {
     return pose;
