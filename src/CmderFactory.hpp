6150a4af5e7efe3f143abd6b33185d734b784e17
Rayfxl <ray133540@gmail.com>
2024-12-21 20:48:31 +0800

    support turnround

--- src/CmderFactory.hpp
+++ src/CmderFactory.hpp
@@ -2,6 +2,7 @@
 #include <functional>
 #include <list>
 #include <unordered_map>
+#include <string_view>
 #include "Command.hpp"
 #include "ActionGroup.hpp"
 
@@ -18,11 +19,16 @@ public:
     CmderFactory& operator=(const CmderFactory&) noexcept = delete;
 public:
     CmderList GetCmders(const std::string& commands) const noexcept;
+
+private:
+    std::string ParseCommandString(std::string_view commands) const noexcept;
+    void ReplaceAll(std::string& inout, std::string_view what, std::string_view with) const noexcept;
+
 private:
     const std::unordered_map<char, Cmder> cmderMap{
         {'M', MoveCommand()}, {'L', TurnLeftCommand()},
         {'R', TurnRightCommand()}, {'F', FastCommand()}, 
-        {'B', ReverseCommand()}, 
+        {'B', ReverseCommand()}, {'Z', TurnRoundCommand()},
     };
 };
 }// namespace adas
